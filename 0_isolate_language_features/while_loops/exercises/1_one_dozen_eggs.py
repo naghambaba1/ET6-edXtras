580f9da340dcbeb89e1d8acc80fcf1a29b041818
Nagham al baba <142064107+naghambaba1@users.noreply.github.com>
2025-02-08 23:13:16 +0200

    Update 1_one_dozen_eggs.py( filled the blanks )

--- 0_isolate_language_features/while_loops/exercises/1_one_dozen_eggs.py
+++ 0_isolate_language_features/while_loops/exercises/1_one_dozen_eggs.py
@@ -3,8 +3,8 @@ print('-- begin --')
 # Fill in the blanks to pass the assertions
 number_of_eggs = 0
 
-while number_of_eggs != _:
-    number_of_eggs = number_of_eggs + _
+while number_of_eggs != 12:
+    number_of_eggs = number_of_eggs + 1
     print('number_of_eggs:', number_of_eggs)
 
 assert number_of_eggs == 12, 'there should be a dozen eggs'
